b53346c3288b5e2b431e7f87c562b9260e7a687f
Ondicho <ondicho53@gmail.com>
2020-10-12 05:28:56 +0300

    sets an onclick listener to navigate from main activity to farm smart activiy

--- app/src/main/java/com/moringaschool/farmsmart/MainActivity.java
+++ app/src/main/java/com/moringaschool/farmsmart/MainActivity.java
@@ -2,13 +2,27 @@ package com.moringaschool.farmsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFarmSmartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFarmSmartButton=(Button)findViewById(R.id.farmSmartButton);
+        mFarmSmartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, FarmSmartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
