b58d3f09a96137d0d227e1a14d549b2485aa7cf1
Ondicho <ondicho53@gmail.com>
2020-10-12 06:22:05 +0300

    adds a   button to farmsmart activity

--- app/src/main/java/com/moringaschool/farmsmart/FarmSmartActivity.java
+++ app/src/main/java/com/moringaschool/farmsmart/FarmSmartActivity.java
@@ -2,13 +2,21 @@ package com.moringaschool.farmsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class FarmSmartActivity extends AppCompatActivity {
+
+    private EditText mCropEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_smart);
+
+        Intent intent = getIntent();
+
+
     }
 }
