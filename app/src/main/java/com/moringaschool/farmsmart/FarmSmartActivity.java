d5cf78171a1385f60330707de3d93c7d5820c6f1
Ondicho <ondicho53@gmail.com>
2020-10-12 06:32:10 +0300

    creates new activity farm procedure and its corresponding xml file

--- app/src/main/java/com/moringaschool/farmsmart/FarmSmartActivity.java
+++ app/src/main/java/com/moringaschool/farmsmart/FarmSmartActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class FarmSmartActivity extends AppCompatActivity {
 
     private EditText mCropEditText;
+    private Button mEditCropButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,15 @@ public class FarmSmartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_farm_smart);
 
         Intent intent = getIntent();
+
+        mEditCropButton=(Button)findViewById(R.id.editCropButton);
+        mEditCropButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent=new Intent(FarmSmartActivity.this,FarmProcedureActivity.class);
+
+            }
+        });
 
 
     }
