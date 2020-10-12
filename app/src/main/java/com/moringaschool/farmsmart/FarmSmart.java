d1d5e9adadca67c3aa833de5312074cd57ff6d07
Ondicho <ondicho53@gmail.com>
2020-10-12 05:21:41 +0300

    creates a new empty activity farm smart and its corresponding xml file

---
+++ app/src/main/java/com/moringaschool/farmsmart/FarmSmart.java
@@ -0,0 +1,14 @@
+package com.moringaschool.farmsmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmSmart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm_smart);
+    }
+}
