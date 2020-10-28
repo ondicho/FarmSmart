4549560e210aef1556ba4c265c0c153dc5389957
ondicho <ondicho53@gmail.com>
2020-10-28 14:19:46 +0300

    pass searched crop as an argument using database reference method

--- app/src/main/java/com/moringaschool/farmsmart/FarmSmartActivity.java
+++ app/src/main/java/com/moringaschool/farmsmart/FarmSmartActivity.java
@@ -8,6 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.Constants;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -16,12 +22,20 @@ public class FarmSmartActivity extends AppCompatActivity  implements View.OnClic
 
     @BindView(R.id.cropEditText) EditText mCropEditText;
     @BindView(R.id.editCropButton) Button mEditCropButton;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
+    private DatabaseReference mSearchedCropReference;
     private static final String TAG = "FarmSmartActivity";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mSearchedCropReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_CROP);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_smart);
         ButterKnife.bind(this);
@@ -34,11 +48,17 @@ public class FarmSmartActivity extends AppCompatActivity  implements View.OnClic
         public void onClick(View v) {
             if(v == mEditCropButton) {
                 String userInput = mCropEditText.getText().toString();
+                //saving location to firebase
+                saveUserInputToFirebase(userInput);
                 Intent intent = new Intent(FarmSmartActivity.this, FarmProcedureActivity.class);
                 intent.putExtra("userInput",userInput);
                 startActivity(intent);
 
             }
+        }
+
+        public void saveUserInputToFirebase(String userInput){
+        mSearchedCropReference.setValue(userInput);
         }
 
 
