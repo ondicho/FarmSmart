ac9c73b97f7c993fe41c6f4b51a003c4ea946a74
ondicho <ondicho53@gmail.com>
2020-11-03 05:55:34 +0300

    makes changes to the file Constants.java

--- app/src/main/java/com/Constants.java
+++ app/src/main/java/com/Constants.java
@@ -1,7 +1,8 @@
 package com;
 
 public class Constants {
-    public static final String TREFLE_BASE_URL = "https://trefle.io/";
+    public static final String TREFLE_LEFT_URL = "https://trefle.io/api/v1/plants?token=";
+    public static final String TREFLE_RIGHT_URL="&filter%5Bcommon_name%5D=beach%20";
     public static final String TREFLE_API_KEY = "_ubFBW9pJVcNZP-kRJqjF0Q_obxMbzbZmvG9lmXP3R4";
 
     public static final String FIREBASE_CHILD_SEARCHED_CROP = "searchedCrop";
