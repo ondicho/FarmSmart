d1770afd1359924bc3270b88602c3cc8ce7cff5a
ondicho <ondicho53@gmail.com>
2020-11-04 19:25:55 +0300

    makes changes to farmdetail fragment

--- app/src/main/java/com/moringaschool/farmsmart/FarmDetailFragment.java
+++ app/src/main/java/com/moringaschool/farmsmart/FarmDetailFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,7 +19,6 @@ import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
-import butterknife.Bind;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 /**
@@ -29,51 +30,36 @@ public class FarmDetailFragment extends Fragment {
     @BindView(R.id.searchedCropimageView) ImageView mImageLabel;
     @BindView(R.id.commonNameTextView) TextView mCommonNameLabel;
     @BindView(R.id.scientificNameTextView) TextView mScientificNameLabel;
+    @BindView(R.id.saveSearchedCropButton) Button mSaveSearchedCropLabel;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Datum mCrop;
 
     public FarmDetailFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FarmDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FarmDetailFragment newInstance(String param1, String param2) {
-        FarmDetailFragment fragment = new FarmDetailFragment();
+    public static FarmDetailFragment newInstance(Datum crop) {
+        FarmDetailFragment farmDetailFragment = new FarmDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putParcelable("crop",Parcels.wrap(crop));
+        farmDetailFragment.setArguments(args);
+        return farmDetailFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+       mCrop= Parcels.unwrap(getArguments().getParcelable("crop"));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_farm_detail, container, false);
+        View view=inflater.inflate(R.layout.fragment_farm_detail,container,false);
+        ButterKnife.bind(this,view);
+        Picasso.get().load(mCrop.getImageUrl()).into(mImageLabel);
+        mCommonNameLabel.setText(mCrop.getCommonName());
+        mScientificNameLabel.setText(mCrop.getScientificName());
+        return view;
     }
 }
