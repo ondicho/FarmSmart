ad0e15ba6e591f75fb48bf83719f44e90ebba5df
ondicho <ondicho53@gmail.com>
2020-11-04 12:45:46 +0300

    creates a value event lisetener for firebase searched crop reference

--- app/src/main/java/com/Constants.java
+++ app/src/main/java/com/Constants.java
@@ -2,7 +2,7 @@ package com;
 
 public class Constants {
     public static final String TREFLE_LEFT_URL = "https://trefle.io/api/v1/plants?";
-    public static final String TREFLE_RIGHT_URL="&filter%5Bcommon_name%5D=beach%20";
+    public static final String TREFLE_RIGHT_URL="&filter%5Bcommon_name%5D=";
     public static final String TREFLE_API_KEY = "_ubFBW9pJVcNZP-kRJqjF0Q_obxMbzbZmvG9lmXP3R4";
 
     public static final String FIREBASE_CHILD_SEARCHED_CROP = "searchedCrop";
--- app/src/main/java/com/moringaschool/farmsmart/FarmSmartActivity.java
+++ app/src/main/java/com/moringaschool/farmsmart/FarmSmartActivity.java
@@ -1,9 +1,11 @@
 package com.moringaschool.farmsmart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,8 +13,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.Constants;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,19 +27,34 @@ public class FarmSmartActivity extends AppCompatActivity  implements View.OnClic
 
     @BindView(R.id.cropEditText) EditText mCropEditText;
     @BindView(R.id.editCropButton) Button mEditCropButton;
-//    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     private DatabaseReference mSearchedCropReference;
     private static final String TAG = "FarmSmartActivity";
-
+    private ValueEventListener  mSearchedCropReferenceListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        mSearchedCropReference = FirebaseDatabase
-//                .getInstance()
-//                .getReference()
-//                .child(Constants.FIREBASE_CHILD_SEARCHED_CROP);
+        mSearchedCropReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_CROP);
+
+        mSearchedCropReferenceListener=mSearchedCropReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for(DataSnapshot userInputSnapshot: snapshot.getChildren()){
+                    String userInput=userInputSnapshot.getValue().toString();
+                    Log.d("Searched crops updated","Searched Crop:" + userInput);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_smart);
@@ -43,13 +63,15 @@ public class FarmSmartActivity extends AppCompatActivity  implements View.OnClic
         Intent intent = getIntent();
 
         mEditCropButton.setOnClickListener(this);
+
+
     }
         @Override
         public void onClick(View v) {
             if(v == mEditCropButton) {
                 String userInput = mCropEditText.getText().toString();
                 //saving location to firebase
-//                saveUserInputToFirebase(userInput);
+                saveUserInputToFirebase(userInput);
                 Intent intent = new Intent(FarmSmartActivity.this, FarmProcedureActivity.class);
                 intent.putExtra("userInput",userInput);
                 startActivity(intent);
@@ -57,10 +79,16 @@ public class FarmSmartActivity extends AppCompatActivity  implements View.OnClic
             }
         }
 
-//        public void saveUserInputToFirebase(String userInput){
-//        //pass user input as argument to set value in firebase db
-//        mSearchedCropReference.push().setValue(userInput);
-//        }
+        public void saveUserInputToFirebase(String userInput){
+        //pass user input as argument to set value in firebase db
+        mSearchedCropReference.push().setValue(userInput);
+        }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSearchedCropReference.removeEventListener(mSearchedCropReferenceListener);
+    }
 
 
 }
