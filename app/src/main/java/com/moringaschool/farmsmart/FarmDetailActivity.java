fa2551a97910134c304b13b0fa99b4b8deb6a823
ondicho <ondicho53@gmail.com>
2020-11-05 21:30:26 +0300

    implement fragment page adapter

--- app/src/main/java/com/moringaschool/farmsmart/FarmDetailActivity.java
+++ app/src/main/java/com/moringaschool/farmsmart/FarmDetailActivity.java
@@ -1,5 +1,38 @@
 package com.moringaschool.farmsmart;
 
+import android.app.FragmentManager;
+import android.os.Bundle;
 
-public class FarmDetailActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.adapters.CropPagerAdapter;
+import com.models.Datum;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+public class FarmDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
+    private CropPagerAdapter adapterViewPager;
+    List<Datum> mCrops;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm_detail);
+        ButterKnife.bind(this);
+
+        mCrops = Parcels.unwrap(getIntent().getParcelableExtra("crops"));
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new CropPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mCrops);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
+    }
 }
