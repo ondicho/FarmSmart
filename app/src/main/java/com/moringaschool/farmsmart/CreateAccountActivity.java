1be1b4d9bf12bd9f277b26974b36e432908f8d56
ondicho <ondicho53@gmail.com>
2020-11-06 07:17:02 +0300

    implements navigation through the log in and signup pages using intents

--- app/src/main/java/com/moringaschool/farmsmart/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/farmsmart/CreateAccountActivity.java
@@ -2,13 +2,55 @@ package com.moringaschool.farmsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.createUserButton)
+    Button mCreateUserButton;
+    @BindView(R.id.nameEditText)
+    EditText mNameEditText;
+    @BindView(R.id.emailEditText)
+    EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText)
+    EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView)
+    TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        {
+
+            if (v == mLoginTextView) {
+                Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+
+            if (v == mCreateUserButton) {
+                createNewUser();
+            }
+
+        }
     }
 }
--- app/src/main/java/com/moringaschool/farmsmart/LoginActivity.java
+++ app/src/main/java/com/moringaschool/farmsmart/LoginActivity.java
@@ -2,13 +2,33 @@ package com.moringaschool.farmsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==mRegisterTextView){
+            Intent intent=new Intent(LoginActivity.this,CreateAccountActivity.class);
+
+            startActivity(intent);
+            finish();
+        }
     }
 }
